ae14d4f5be4f7102851bf34fd75c79c0ff3f6532
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-04-23 22:02:00 -0300

    Format binary search algorithm

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,19 +1,21 @@
-export default function bs_list(haystack: number[], needle: number): boolean {
-  let low = 0;
-  let high = haystack.length;
+export default function binarySearch(haystack: number[], needle: number): boolean {
+	let low = 0;
+	let high = haystack.length;
 
-  do {
-    const middle_point = Math.floor(low + (high - low) / 2);
-    const value = haystack[middle_point];
+	do {
+		const middlePoint = Math.floor((low + (high - low)) / 2);
+		const value = haystack[middlePoint];
 
-    if (value === needle) {
-      return true;
-    } else if (value > needle) {
-      high = middle_point;
-    } else {
-      low = middle_point + 1;
-    }
-  } while (low < high);
+		if (value === needle) {
+			return true;
+		}
 
-  return false;
+		if (value > needle) {
+			high = middlePoint;
+		} else {
+			low = middlePoint + 1;
+		}
+	} while (low < high);
+
+	return false;
 }
