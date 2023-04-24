ba1159761b9de9fa410c4ed8b2ec7bc8060d3063
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-04-23 22:22:05 -0300

    Format linear search and use for-of

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,9 +1,9 @@
-export default function linear_search(haystack: number[], needle: number): boolean {
-  for (let i = 0; i < haystack.length; ++i) {
-    if (haystack[i] === needle) {
-      return true;
-    }
-  }
+export default function linearSearch(haystack: number[], needle: number): boolean {
+	for (const i of haystack) {
+		if (haystack[i] === needle) {
+			return true;
+		}
+	}
 
-  return false;
+	return false;
 }
